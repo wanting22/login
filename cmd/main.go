41b0f2cf1669a25dadac00266413b7dbc2fb98ce
Sandy <nicesandy21419@yahoo.com.tw>
2022-12-01 18:18:42 +0800

    modify html、css  path

--- cmd/main.go
+++ cmd/main.go
@@ -11,9 +11,9 @@ func main() {
 	r.GET("/login", auth.LoginPage)
 	r.POST("/login", auth.LoginAuth)
 	//讀取html的所在位置 ＊是代表目錄所有檔案
-	r.LoadHTMLGlob("login/web/html/*")
+	r.LoadHTMLGlob("../web/html/*")
 	//設定靜態資源的讀取
 	//https://matthung0807.blogspot.com/2021/07/go-gin-static-files.html
-	r.Static("/css", "./login/web/css")
+	r.Static("/css", "../web/css")
 	r.Run(":8000")
 }
